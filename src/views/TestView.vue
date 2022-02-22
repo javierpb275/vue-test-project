95b8b1280d7fc3d0c21103665ad616f1a18782d7
javierpb275 <javierpb275@gmail.com>
2022-02-22 19:58:59 +0100

    template syntax

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,9 +1,32 @@
 <script setup>
-
+const hello = false;
+const googleLink = "https://www.google.com/";
+const saySth = () => {
+  if (hello) {
+    alert("hello");
+  } else {
+    alert("bye");
+  }
+};
+const someAttribute = 'href';
+const someUrl = "https://www.google.com/";
+const someEvent = 'focus';
+const onSubmit = () => {
+  alert('submitted form!')
+}
 </script>
 
 <template>
   <main>
-    TEST
+    <p v-if="hello">hello</p>
+    <p v-else>bye</p>
+    <a :href="googleLink">google</a>
+    <br>
+    <a :[someAttribute]="someUrl">some place</a>
+    <p @click="saySth">say sth</p>
+    <input @[someEvent]="saySth" type="text"/>
+    <form @submit.prevent="onSubmit">
+      <button type="submit">submit form</button>
+    </form>
   </main>
 </template>
