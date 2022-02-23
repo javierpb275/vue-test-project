138f8739676b4323b234a0441f62e19adf0d6ee5
javierpb275 <javierpb275@gmail.com>
2022-02-23 18:47:33 +0100

    computed properties

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,17 +1,41 @@
 <script setup>
-import { ref } from "vue";
+import { reactive, computed, ref } from "vue";
 
-const count = ref(0);
+const author = reactive({
+  name: "John Doe",
+  books: [
+    "Vue 2 - Advanced Guide",
+    "Vue 3 - Basic Guide",
+    "Vue 4 - The Mystery",
+  ],
+});
 
-function increment() {
-  count.value++;
-}
+// a computed ref
+const publishedBooksMessage = computed(() => {
+  return author.books.length > 0 ? "Yes" : "No";
+});
+
+const firstName = ref("John");
+const lastName = ref("Doe");
+
+const fullName = computed({
+  // getter
+  get() {
+    return firstName.value + " " + lastName.value;
+  },
+  // setter
+  set(newValue) {
+    // Note: we are using destructuring assignment syntax here.
+    [firstName.value, lastName.value] = newValue.split(" ");
+  },
+});
 </script>
 
 <template>
   <main>
-    <button @click="increment">
-      {{ count }}
-    </button>
+    <p>Has published books:</p>
+    <span>{{ publishedBooksMessage }}</span>
+    <br>
+    <span>{{fullName}}</span>
   </main>
 </template>
