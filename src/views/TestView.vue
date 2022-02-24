a1d3b2b638c281f950a74a6c125515e8f6d00ff2
javierpb275 <javierpb275@gmail.com>
2022-02-24 15:35:00 +0100

    lifecycle hooks

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,38 +1,21 @@
 <script setup>
-import { reactive, computed, ref } from "vue";
-const toggle = ref("yes");
+import { reactive, computed, ref, onMounted, onUpdated, onUnmounted } from "vue";
 
-const toggle2 = ref("TRUE");
-const dynamicFalseValue = ref("FALSE");
-const dynamicTrueValue = ref("TRUE");
+onMounted(() => {
+  console.log(`hello`);
+});
 
-const pick = ref("two");
-const first = ref("one");
-const second = ref("two");
+const count = ref(0);
 
-const selected = reactive({ number: 321 });
+onUpdated(() => {
+  console.log(count.value);
+});
+
+onUnmounted(() => console.log(`goodbye`))
 </script>
 
 <template>
   <main>
-    <input type="checkbox" v-model="toggle" true-value="yes" false-value="no" />
-    <span>{{ toggle }}</span>
-    <input
-      type="checkbox"
-      v-model="toggle2"
-      :true-value="dynamicTrueValue"
-      :false-value="dynamicFalseValue"
-    />
-    <span>{{ toggle2 }}</span>
-
-    <input type="radio" v-model="pick" :value="first" />
-    <input type="radio" v-model="pick" :value="second" />
-    <span>{{ pick }}</span>
-
-    <select v-model="selected">
-      <option :value="{ number: 123 }">123</option>
-      <option :value="{ number: 321 }">321</option>
-    </select>
-    <span>{{ selected.number }}</span>
+    <button id="count" @click="count++">{{ count }}</button>
   </main>
 </template>
