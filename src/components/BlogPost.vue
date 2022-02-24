8c906729ffd10dbe654a8533972f3afbea4396a2
javierpb275 <javierpb275@gmail.com>
2022-02-24 17:27:33 +0100

    components: define and use components and pass props

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,7 @@
+<script setup>
+const props = defineProps(["title"]);
+</script>
+
+<template>
+  <h4>{{ props.title }}</h4>
+</template>
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,42 +1,18 @@
 <script setup>
-import { ref, reactive, watch } from "vue";
+import CountButton from "../components/CountButton.vue";
+import BlogPost from "../components/BlogPost.vue";
+import { ref } from "vue";
 
-const x = ref(0);
-const y = ref(0);
-
-// single ref
-watch(x, (newX) => {
-  console.log(`x is ${newX}`);
-});
-
-// getter
-watch(
-  () => x.value + y.value,
-  (sum) => {
-    console.log(`sum of x + y is: ${sum}`);
-  }
-);
-
-// array of multiple sources
-watch([x, () => y.value], ([newX, newY]) => {
-  console.log(`x is ${newX} and y is ${newY}`);
-});
-
-const obj = reactive({ count: 0 })
-
-watch(
-  () => obj.count,
-  (count) => {
-    console.log(`count is: ${count}`)
-  }
-)
+const posts = ref([
+  { id: 1, title: "My journey with Vue" },
+  { id: 2, title: "Blogging with Vue" },
+  { id: 3, title: "Why Vue is so fun" },
+]);
 </script>
 
 <template>
   <main>
-    <input type="text" v-model.number="x" />
-    <input type="text" v-model.number="y" />
-    <br>
-    <input type="text" v-model.number="obj.count" />
+    <CountButton />
+    <BlogPost v-for="post in posts" :key="post.id" :title="post.title" />
   </main>
 </template>
