c3b8c64e3fc5fe2f5ac20382c8f1dda6494275c5
javierpb275 <javierpb275@gmail.com>
2022-02-24 14:17:13 +0100

    form input bindings: checkbox and radio button

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,16 +1,35 @@
 <script setup>
 import { reactive, computed, ref } from "vue";
 
-const message = ref("");
+const checked = ref(true);
+
+const checkedNames = ref([]);
+
+const picked = ref("");
 </script>
 
 <template>
   <main>
-    <p>Message is: {{ message }}</p>
-    <input v-model="message" placeholder="edit me" />
-    <br>
-    <span>Multiline message is:</span>
-    <p style="white-space: pre-line">{{ message }}</p>
-    <textarea v-model="message" placeholder="add multiple lines"></textarea>
+    <input type="checkbox" id="checkbox" v-model="checked" />
+    <label for="checkbox">{{ checked }}</label>
+
+    <div>Checked names: {{ checkedNames }}</div>
+
+    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
+    <label for="jack">Jack</label>
+
+    <input type="checkbox" id="john" value="John" v-model="checkedNames" />
+    <label for="john">John</label>
+
+    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
+    <label for="mike">Mike</label>
+
+    <div>Picked: {{ picked }}</div>
+
+    <input type="radio" id="one" value="One" v-model="picked" />
+    <label for="one">One</label>
+
+    <input type="radio" id="two" value="Two" v-model="picked" />
+    <label for="two">Two</label>
   </main>
 </template>
