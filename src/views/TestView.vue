ec855176dc4366bc1d055d9ef6bf61e9e35f9653
javierpb275 <javierpb275@gmail.com>
2022-02-24 14:47:33 +0100

    form input bindings: value bindings

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,23 +1,38 @@
 <script setup>
 import { reactive, computed, ref } from "vue";
+const toggle = ref("yes");
 
-const selected = ref("A");
+const toggle2 = ref("TRUE");
+const dynamicFalseValue = ref("FALSE");
+const dynamicTrueValue = ref("TRUE");
 
-const options = ref([
-  { text: "One", value: "A" },
-  { text: "Two", value: "B" },
-  { text: "Three", value: "C" },
-]);
+const pick = ref("two");
+const first = ref("one");
+const second = ref("two");
+
+const selected = reactive({ number: 321 });
 </script>
 
 <template>
   <main>
-    <select v-model="selected">
-      <option v-for="option in options" :value="option.value" :key="option">
-        {{ option.text }}
-      </option>
-    </select>
+    <input type="checkbox" v-model="toggle" true-value="yes" false-value="no" />
+    <span>{{ toggle }}</span>
+    <input
+      type="checkbox"
+      v-model="toggle2"
+      :true-value="dynamicTrueValue"
+      :false-value="dynamicFalseValue"
+    />
+    <span>{{ toggle2 }}</span>
 
-    <div>Selected: {{ selected }}</div>
+    <input type="radio" v-model="pick" :value="first" />
+    <input type="radio" v-model="pick" :value="second" />
+    <span>{{ pick }}</span>
+
+    <select v-model="selected">
+      <option :value="{ number: 123 }">123</option>
+      <option :value="{ number: 321 }">321</option>
+    </select>
+    <span>{{ selected.number }}</span>
   </main>
 </template>
