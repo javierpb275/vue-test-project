615adb36d523bc34993e2519a01ccd1dac379484
javierpb275 <javierpb275@gmail.com>
2022-02-24 14:27:44 +0100

    form input bindings: select

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,35 +1,23 @@
 <script setup>
 import { reactive, computed, ref } from "vue";
 
-const checked = ref(true);
+const selected = ref("A");
 
-const checkedNames = ref([]);
-
-const picked = ref("");
+const options = ref([
+  { text: "One", value: "A" },
+  { text: "Two", value: "B" },
+  { text: "Three", value: "C" },
+]);
 </script>
 
 <template>
   <main>
-    <input type="checkbox" id="checkbox" v-model="checked" />
-    <label for="checkbox">{{ checked }}</label>
+    <select v-model="selected">
+      <option v-for="option in options" :value="option.value" :key="option">
+        {{ option.text }}
+      </option>
+    </select>
 
-    <div>Checked names: {{ checkedNames }}</div>
-
-    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
-    <label for="jack">Jack</label>
-
-    <input type="checkbox" id="john" value="John" v-model="checkedNames" />
-    <label for="john">John</label>
-
-    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
-    <label for="mike">Mike</label>
-
-    <div>Picked: {{ picked }}</div>
-
-    <input type="radio" id="one" value="One" v-model="picked" />
-    <label for="one">One</label>
-
-    <input type="radio" id="two" value="Two" v-model="picked" />
-    <label for="two">Two</label>
+    <div>Selected: {{ selected }}</div>
   </main>
 </template>
