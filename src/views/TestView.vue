a6af0bb3c96571df9639878b5a84671f679124b6
javierpb275 <javierpb275@gmail.com>
2022-02-24 14:08:29 +0100

    form input bindings: input text and textarea

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,39 +1,16 @@
 <script setup>
 import { reactive, computed, ref } from "vue";
 
-const name = ref("Vue.js");
-
-function greet(event) {
-  alert(`Hello ${name.value}!`);
-  if (event) {
-    alert(event.target.tagName);
-  }
-}
-
-function say(message) {
-  alert(message);
-}
-
-function warn(message, event) {
-  if (event) {
-    event.preventDefault();
-  }
-  alert(message);
-}
+const message = ref("");
 </script>
 
 <template>
   <main>
-    <button @click="greet">Greet</button>
-    <button @click="say('hello')">Say hello</button>
-    <button @click="say('bye')">Say bye</button>
-    <button @click="warn('Form cannot be submitted yet.', $event)">
-      Submit
-    </button>
-    <button @click="(event) => warn('Form cannot be submitted yet.', event)">
-      Submit
-    </button>
-    <form @submit.prevent="onSubmit"></form>
-    <form @submit.prevent></form>
+    <p>Message is: {{ message }}</p>
+    <input v-model="message" placeholder="edit me" />
+    <br>
+    <span>Multiline message is:</span>
+    <p style="white-space: pre-line">{{ message }}</p>
+    <textarea v-model="message" placeholder="add multiple lines"></textarea>
   </main>
 </template>
