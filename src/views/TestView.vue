2b61fa00f1b9d4589a7931082b3cb638cb62a5c9
javierpb275 <javierpb275@gmail.com>
2022-02-25 13:06:02 +0100

    using the store

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,35 +1,16 @@
 <script setup>
-import BaseLayout from "../components/BaseLayout.vue";
+import { useCounterStore } from "@/stores/counter";
+import { storeToRefs } from "pinia";
+
+const counterStore = useCounterStore();
+const { increment } = counterStore;
+const { counter, doubleCount } = storeToRefs(counterStore);
 </script>
 
 <template>
   <main>
-    <BaseLayout>
-      <template #header>
-        <h1>Here might be a page title</h1>
-      </template>
-
-      <template #default>
-        <p>A paragraph for the main content.</p>
-        <p>And another one.</p>
-      </template>
-
-      <template #footer>
-        <p>Here's some contact info</p>
-      </template>
-    </BaseLayout>
-    <BaseLayout>
-      <template #header>
-        <h1>Here might be a page title</h1>
-      </template>
-
-      <!-- implicit default slot -->
-      <p>A paragraph for the main content.</p>
-      <p>And another one.</p>
-
-      <template #footer>
-        <p>Here's some contact info</p>
-      </template>
-    </BaseLayout>
+    <p>{{ counter }}</p>
+    <p>{{ doubleCount }}</p>
+    <button @click="increment">+1</button>
   </main>
 </template>
