a251c54c3a57417307fae4a583242cff069e41f3
javierpb275 <javierpb275@gmail.com>
2022-02-25 13:42:52 +0100

    state

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,10 +1,19 @@
 <script setup>
 import { useCounterStore } from "@/stores/counter";
 import { storeToRefs } from "pinia";
+import { ref } from "vue";
 
 const counterStore = useCounterStore();
 const { increment } = counterStore;
-const { counter, doubleCount } = storeToRefs(counterStore);
+const { counter, doubleCount, name } = storeToRefs(counterStore);
+
+const inputName = ref("");
+
+const changeName = (newName) => {
+  counterStore.$patch((state) => {
+    state.name = newName;
+  });
+};
 </script>
 
 <template>
@@ -12,5 +21,11 @@ const { counter, doubleCount } = storeToRefs(counterStore);
     <p>{{ counter }}</p>
     <p>{{ doubleCount }}</p>
     <button @click="increment">+1</button>
+    <hr />
+    <p>{{ name }}</p>
+    <input type="text" placeholder="change name..." v-model="inputName" />
+    <button @click="changeName(inputName)">change name</button>
+    <hr />
+    <button @click="counterStore.$reset()">reset</button>
   </main>
 </template>
